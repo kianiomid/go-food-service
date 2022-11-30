04b7d69e33914df7d6f140035413a3403f9db311
OmidKiani <kianiomid11@gmail.com>
2022-11-30 15:05:36 +0330

    created user and food repositories

---
+++ internal/domain/repository/food_repository.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"food-service/internal/domain/entity"
+	"github.com/jinzhu/gorm"
+)
+
+type FoodRepository struct {
+	db *gorm.DB
+}
+
+type IFoodRepository interface {
+	SaveFood(*entity.Food) (*entity.Food, error)
+	FindFoodById(int) (*entity.Food, error)
+	GetAllFood() ([]entity.Food, error)
+	UpdateFood(*entity.Food) (*entity.Food, error)
+	DeleteFoodById(int) error
+}
+
+func NewFoodRepository(db *gorm.DB) *FoodRepository {
+	//return &FoodRepository{db}
+	var foodRepository = FoodRepository{}
+	foodRepository.db = db
+	return &foodRepository
+}
+
+func (foodRepository *FoodRepository) SaveFood(food *entity.Food) (*entity.Food, error) {
+	err := foodRepository.db.Create(&food).Error
+	if err != nil {
+		return nil, err
+	}
+	return food, nil
+}
+
+func (foodRepository *FoodRepository) FindFoodById(id int) (*entity.Food, error) {
+	var food entity.Food
+	err := foodRepository.db.Where("id = ?", id).Take(&food).Error
+	if err != nil {
+		return nil, err
+	}
+	return &food, nil
+}
+
+func (foodRepository *FoodRepository) GetAllFood() ([]entity.Food, error) {
+	var foods []entity.Food
+	err := foodRepository.db.Order("id desc").Find(&foods).Error
+	if err != nil {
+		return nil, err
+	}
+	return foods, nil
+}
+
+func (foodRepository *FoodRepository) UpdateFood(food *entity.Food) (*entity.Food, error) {
+	err := foodRepository.db.Save(&food).Error
+	if err != nil {
+		return nil, err
+	}
+	return food, nil
+}
+
+func (foodRepository *FoodRepository) DeleteFoodById(id int) error {
+	var food entity.Food
+	err := foodRepository.db.Where("id = ?", id).Delete(&food).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/domain/repository/user_repository.go
@@ -0,0 +1,87 @@
+package repository
+
+import (
+	"fmt"
+	"food-service/internal/domain/dto"
+	"food-service/internal/domain/entity"
+	"github.com/jinzhu/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+type IUserRepository interface {
+	SaveUser(*entity.User) (*entity.User, error)
+	FindUserById(int) (*entity.User, error)
+	GetAllUser() ([]entity.User, error)
+	UpdateUser(*entity.User) (*entity.User, error)
+	DeleteUserById(int) error
+	GetUserNameById(id int) string
+	GetUserByEmailPassword(loginViewModel dto.LoginViewModel) (*entity.User, error)
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	//return &UserRepository{db}
+	var userRepository = UserRepository{}
+	userRepository.db = db
+	return &userRepository
+}
+
+func (userRepository *UserRepository) SaveUser(user *entity.User) (*entity.User, error) {
+	err := userRepository.db.Create(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (userRepository *UserRepository) FindUserById(id int) (*entity.User, error) {
+	var user entity.User
+	err := userRepository.db.Where("id = ?", id).Take(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (userRepository *UserRepository) GetUserNameById(id int) string {
+	userDetail, _ := userRepository.FindUserById(id)
+	var fullname = fmt.Sprintf("%s %s", userDetail.FirstName, userDetail.LastName)
+	return fullname
+}
+
+func (userRepository *UserRepository) GetAllUser() ([]entity.User, error) {
+	var users []entity.User
+	err := userRepository.db.Order("id desc").Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (userRepository *UserRepository) UpdateUser(user *entity.User) (*entity.User, error) {
+	err := userRepository.db.Save(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (userRepository *UserRepository) DeleteUserById(id int) error {
+	var user entity.User
+	err := userRepository.db.Where("id = ?", id).Delete(&user).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (userRepository *UserRepository) GetUserByEmailPassword(loginViewModel dto.LoginViewModel) (*entity.User, error) {
+	var user entity.User
+	err := userRepository.db.Where("email = ?", loginViewModel.Email).Take(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}

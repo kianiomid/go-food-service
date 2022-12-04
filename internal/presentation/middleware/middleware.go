dfd497bac5bba298d94a287ee004fdaef23b8100
OmidKiani <kianiomid11@gmail.com>
2022-12-04 12:28:09 +0330

    created middleware for Auth and CORS

---
+++ internal/presentation/middleware/middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"food-service/pkg/jwttoken"
+	"food-service/pkg/response"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := jwttoken.TokenValid(c.Request)
+		if err != nil {
+			response.ResponseError(c, err.Error(), http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Method", "POST, GET, DELETE, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
